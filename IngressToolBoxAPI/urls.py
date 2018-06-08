0369970ee25e95ea6799af112fdf3d7babf9eb59
bllli <me@bllli.cn>
2018-06-08 20:18:56 +0800

    项目换个名字大家说吼不吼啊?

--- IngressToolBoxAPI/urls.py
+++ IngressToolBoxAPI/urls.py
@@ -1,4 +1,4 @@
-"""ingress_passcodes_share URL Configuration
+"""IngressToolBoxAPI URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.0/topics/http/urls/
--- IngressToolBoxAPI/wsgi.py
+++ IngressToolBoxAPI/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for ingress_passcodes_share project.
+WSGI config for IngressToolBoxAPI project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ingress_passcodes_share.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "IngressToolBoxAPI.settings")
 
 application = get_wsgi_application()
